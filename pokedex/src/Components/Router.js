faed03de14a8a5f5aef012c047ca3ccd34795a78
Ariane Lopes de Camargo <arianee.lopees@hotmail.com>
2022-09-01 21:52:37 -0300

    aripokedex

--- pokedex/src/Components/Router.js
+++ pokedex/src/Components/Router.js
@@ -2,13 +2,14 @@ import React from "react";
 import Home from "./Home"
 import Pokedex from "./Pokedex"
 import DetalhesPokemon from "./DetalhePokemon"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function Router () {
     return (
         <BrowserRouter>
         <Routes>
-            <Route index element={ <Home/>} />
+            <Route index element={ <Home />} />
             <Route path="/pokedex" element={ <Pokedex/>} />
             <Route path="/detalhes-pokemon" element={ <DetalhesPokemon/>} />
         </Routes>
