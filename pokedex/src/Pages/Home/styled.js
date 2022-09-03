0be08a0e3f0b8814fc6558ef8f70278f0f4e47a3
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-09-02 21:37:06 -0300

    Primeira renderização na Home - V0

---
+++ pokedex/src/Pages/Home/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  width: 80vw;
+  height: 85vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
