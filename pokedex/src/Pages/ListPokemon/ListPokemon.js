25fb6076d12e64062c5e6e65fe15ccaa6581671a
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-09-06 20:28:59 -0300

    corrigir erro de commit

--- pokedex/src/Pages/ListPokemon/ListPokemon.js
+++ pokedex/src/Pages/ListPokemon/ListPokemon.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { CardPokemon } from "../../Components/CardPokemon/CardPokemon";
 import { Header } from "../../Components/Header/Header";
 
-/*function ListPokemon () {
+function ListPokemon () {
 
     const navigate = useNavigate();
     
@@ -19,22 +19,10 @@ import { Header } from "../../Components/Header/Header";
     )
 }
 
-export default ListPokemon;*/
-
-import { CardPokemon } from "../../Components/CardPokemon/CardPokemon";
-import { Header } from "../../Components/Header/Header";
-
-
-
-
-
-export function ListPokemon() {
-    return (
-        <h1>Página Lista de Pokemon (Home)</h1>
-
-       // <Header/>
-       // <CardPokemon/>
-    )
-};
-
 export default ListPokemon;
+
+
+
+
+
+
--- pokedex/src/routes/coordinator.js
+++ pokedex/src/routes/coordinator.js
@@ -2,9 +2,9 @@ export const goToListPokemon = (history) => {
     history.push("/")
 }
 
-/*export const goToDetailsPokemon = (history) => {
+export const goToDetailsPokemon = (history) => {
     history.push("/")
-}*/
+}
 
 /*export const goToPokedex = (history) => {
     history.push("/")
